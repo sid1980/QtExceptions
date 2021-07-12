ce56353c77980a9a367e5ae4efe8cd9365235035
DIODEELEGIA <SyplessEleki@gmail.com>
2021-07-13 02:56:33 +0900

    fixed ci yml

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Syple/Desktop/QtExceptions
-# Build directory: C:/Users/Syple/Desktop/QtExceptions/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Syple/Desktop/QtExceptions/test
-# Build directory: C:/Users/Syple/Desktop/QtExceptions/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(QtExceptionsTest "C:/Users/Syple/Desktop/QtExceptions/build/test/QtExceptionsTest.exe")
-set_tests_properties(QtExceptionsTest PROPERTIES  WORKING_DIRECTORY "C:/Users/Syple/Desktop/QtExceptions/build" _BACKTRACE_TRIPLES "C:/Users/Syple/Desktop/QtExceptions/test/CMakeLists.txt;29;add_test;C:/Users/Syple/Desktop/QtExceptions/test/CMakeLists.txt;0;")
